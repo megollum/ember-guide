573b44eacbf6e42ae7745c7b063c7ee962e2516b
Tarik Eminagic <tarik@east.ba>
2017-05-10 17:44:39 +0200

    returning to initial value

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -26,10 +26,8 @@ test('should toggle wide class on click', function(assert) {
   this.set('rentalObj', rental);
   this.render(hbs`{{rental-listing rental=rentalObj}}`);
   assert.equal(this.$('.image.wide').length, 0, 'initially rendered small');
-  var myImage = this.$('.image');
-  console.log(myImage);
-  myImage.click();
+  this.$('.image').click();
   assert.equal(this.$('.image.wide').length, 1, 'rendered wide after click');
-  myImage.click();
+  this.$('.image').click();
   assert.equal(this.$('.image.wide').length, 0, 'rendered small after second click');
 });
